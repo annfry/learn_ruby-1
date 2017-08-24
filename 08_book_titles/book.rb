08239a2bab9ad470c402d223f19c372489057ad3
Ann Fry <ann.fry@shopify.com>
2017-08-24 11:42:19 -0400

    adding answers for book and timer

--- 08_book_titles/book.rb
+++ 08_book_titles/book.rb
@@ -1,8 +1,26 @@
 class Book
-  def initialize(title)
-    @title = title
-  end
-  def title
-    self.title.capitalize!
+  attr_reader :title
+  def title=(title)
+    if title.split.length >1
+      array = title.split
+      first_word = array.shift
+      first_word.capitalize!
+      array.each { |word| 
+        case word
+          when "i"
+            word.capitalize!
+          when "the"
+          when "a"
+          when "an"
+          when "and"
+          when "in"
+          when "of"
+          else
+              word.capitalize! 
+          end}
+      @title = first_word + " " +  array.join(" ")
+    else
+      @title = title.capitalize
+    end
   end
 end
